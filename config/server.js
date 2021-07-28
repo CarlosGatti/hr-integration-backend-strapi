a7f884099275d8689f3e30536e5b527e9b5cfb14
Carlos Gatti <eduardo.gf@hotmail.com>
2021-07-27 21:14:36 -0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'be12dbb338707f794c191d7e07508da2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd8ed0f80-d22d-49a3-addd-cd82d2c6074c'
+};
